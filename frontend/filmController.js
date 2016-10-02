9cecf22c81663f47c10e616272c619eed919a6bf
Jeff K <admin@thedreamingjeff.com>
2016-10-02 11:13:02 +0200

    and the first controller

---
+++ frontend/filmController.js
@@ -0,0 +1,31 @@
+var logger = new (require('../utils/logger.js'));
+var db = new (require('../utils/Database.js'));
+var _ = require('lodash');
+
+var router = require("express").Router();
+router.route("/films/:id?").get(getFilms).post(addImbdId);
+
+function getFilms(req, res) {
+    logger.info('getting....');
+    db.getAllFilms(function (err, films) {
+        if (err){
+            logger.error('bad');
+            res.send(err);
+        } else {
+            logger.info('good', films.length);
+            res.json(films);
+        }
+    });
+}
+
+function addImbdId(req, res) {
+    var toSave = _.extend({}, req.body);
+    db.updateImdbID(toSave, function (err) {
+        if (err)
+            res.send(err);
+        else
+            res.json(toSave);
+    });
+}
+
+module.exports = router;
