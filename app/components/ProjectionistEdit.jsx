e5889e902187f6a4df039dc7bc7b9144a099ff7a
Jeff K <admin@thedreamingjeff.com>
2016-10-03 17:48:29 +0200

    hide the title when updating on the projectionist

--- app/components/ProjectionistEdit.jsx
+++ app/components/ProjectionistEdit.jsx
@@ -60,7 +60,7 @@ module.exports = React.createClass({
             <form className="updateFilm col-xs-6 form-horizontal" onSubmit={this.addImdbId}>
                 <input type="text" className="form-control" id="title" name="title" value={this.props.info.title} readOnly />
                 <div className="form-group-sm col-xs-7">
-                    <input type="text" className="form-control" id="imdbID" name="imdbID" value={this.state.imdbID} onChange={this.handleInputChange} placeholder="new IMDB id" />
+                    <input type="text" className="form-control hide" id="imdbID" name="imdbID" value={this.state.imdbID} onChange={this.handleInputChange} placeholder="new IMDB id" />
                 </div>
                 <div className="form-group-sm col-xs-4">
                     <button className="btn btn-sm" type="submit">Update Film</button>
