ad16cb848c12f8edeeb86623a8a8000940fff8e3
grandvizier <knurek.j@gmail.com>
2017-09-05 18:59:22 +0200

    cleanup some warnings

--- src/containers/MapShowtimes.js
+++ src/containers/MapShowtimes.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import moment from 'moment'
-const isEmpty = require('lodash/isEmpty');
 const mapObj = require('lodash/map');
 
 const uuidv4 = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
