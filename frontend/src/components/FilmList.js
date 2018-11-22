c9451f416b0587fdf789ad97f1a3721b96312ba7
muhabalwan <alwanmuhab@gmail.com>
2018-11-22 21:10:58 +0100

    Update dependencies and add typescript

--- frontend/src/components/FilmList.js
+++ frontend/src/components/FilmList.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Film from './Film'
-import { Col } from 'react-bootstrap'
-
+import React from "react";
+import PropTypes from "prop-types";
+import Film from "./Film";
+import { Col } from "react-bootstrap";
 
 const FilmList = ({ films, onFilterClick, onFilmSave }) => (
- 	<Col xs={12}>
-		{films.map(film => {
-			if (!film.hidden) {
-				return (<Film key={film._id} {...film}
-						onClick={() => onFilterClick(film._id)}
-						onFilmSave={() => onFilmSave(film._id)} />)
-			}
-			return null
-		})}
-	</Col>
-)
+  <Col xs={12}>
+    {films.map(film => {
+      if (!film.hidden) {
+        return (
+          <Film
+            key={film._id}
+            {...film}
+            onClick={() => onFilterClick(film._id)}
+            onFilmSave={() => onFilmSave(film._id)}
+          />
+        );
+      }
+      return null;
+    })}
+  </Col>
+);
 
 FilmList.propTypes = {
-	films: PropTypes.array.isRequired,
-	onFilterClick: PropTypes.func.isRequired,
-	onFilmSave: PropTypes.func.isRequired
-}
+  films: PropTypes.array.isRequired,
+  onFilterClick: PropTypes.func.isRequired,
+  onFilmSave: PropTypes.func.isRequired
+};
 
-export default FilmList
+export default FilmList;
