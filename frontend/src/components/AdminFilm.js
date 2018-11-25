478ff14f1270746562d82774d3de11f40b2ae1ff
Muhab Alwan <alwanmuhab@gmail.com>
2018-11-25 21:36:35 +0100

    Update dependencies and add typescript (#2)
    * Update dependencies and add typescript
    
    * remove secruity warning from AdminFilm

--- frontend/src/components/AdminFilm.js
+++ frontend/src/components/AdminFilm.js
@@ -1,77 +1,101 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import AdminUpdateFilm from './AdminUpdateFilm'
+import React from "react";
+import PropTypes from "prop-types";
+import AdminUpdateFilm from "./AdminUpdateFilm";
 
-import { Image, Col, Row } from 'react-bootstrap';
+import { Image, Col, Row } from "react-bootstrap";
 
-const AdminFilm = ({ handleSubmit, _id, title, originalID, imdbID, img, details, showtimes, reviewed }) => {
-  let imdbUrl = 'http://www.imdb.com/find?ref_=nv_sr_fn&s=all&q='+title;
-  let filmDetails = (details) ? details : {};
-  let isfilmReviewd = (reviewed) ? true : false;
+const AdminFilm = ({
+  handleSubmit,
+  _id,
+  title,
+  originalID,
+  imdbID,
+  img,
+  details,
+  showtimes,
+  reviewed
+}) => {
+  let imdbUrl = "http://www.imdb.com/find?ref_=nv_sr_fn&s=all&q=" + title;
+  let filmDetails = details ? details : {};
+  let isfilmReviewd = reviewed ? true : false;
   // let showtimesCount = (showtimes.length === 1) ? showtimes[0].timestamp : showtimes.length;
   let showtimesCount = showtimes.length;
   // let origin = (originalID ? 'https://www.berlin.de/kino/_bin/'+originalID : '')
-  let origin = (originalID ? 'https://www.berlin.de'+originalID : '')
-  let altTitle = (filmDetails.aka) ? filmDetails.aka : null
+  let origin = originalID ? "https://www.berlin.de" + originalID : "";
+  let altTitle = filmDetails.aka ? filmDetails.aka : null;
 
   return (
-  <Col xs={9} md={6} className={"editDetails "+ (isfilmReviewd ? 'reviewed' : '')} >
-    <h4>{title} <small>({showtimesCount})</small></h4>
-    {altTitle && <div className="altTitle">{altTitle}</div>}
-    <Row>
-      <Col xs={6} className="details">
-        <span className="type col-xs-6">Current IMDB </span>
-        <span className="value"> {imdbID} </span>
-      </Col>
-      <Col xs={6} className="details">
-        <span className="type col-xs-4">Country</span>
-        <span className="value">{filmDetails.country}</span>
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={6} className="details">
-        <span className="type col-xs-6">Director</span>
-        <span className="value">{filmDetails.director}</span>
-      </Col>
-      <Col xs={6} className="details">
-        <span className="type col-xs-4">Year</span>
-        <span className="value">{filmDetails.year}</span>
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={12} className="details">
-        <span className="type col-xs-2">Actors</span>
-        <span className="value">{filmDetails.actors}</span>
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={12} className="details">
-        <span className="type col-xs-2">Genre</span>
-        <span className="value">{filmDetails.genre}</span>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <a href={imdbUrl} target="_blank">Search IMDB</a>
-      </Col>
-      <Col>
-        <a href={origin} target="_blank">{(originalID ? 'Original data' : '')}</a>
-      </Col>
-    </Row>
+    <Col
+      xs={9}
+      md={6}
+      className={"editDetails " + (isfilmReviewd ? "reviewed" : "")}
+    >
+      <h4>
+        {title} <small>({showtimesCount})</small>
+      </h4>
+      {altTitle && <div className="altTitle">{altTitle}</div>}
+      <Row>
+        <Col xs={6} className="details">
+          <span className="type col-xs-6">Current IMDB </span>
+          <span className="value"> {imdbID} </span>
+        </Col>
+        <Col xs={6} className="details">
+          <span className="type col-xs-4">Country</span>
+          <span className="value">{filmDetails.country}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6} className="details">
+          <span className="type col-xs-6">Director</span>
+          <span className="value">{filmDetails.director}</span>
+        </Col>
+        <Col xs={6} className="details">
+          <span className="type col-xs-4">Year</span>
+          <span className="value">{filmDetails.year}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} className="details">
+          <span className="type col-xs-2">Actors</span>
+          <span className="value">{filmDetails.actors}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} className="details">
+          <span className="type col-xs-2">Genre</span>
+          <span className="value">{filmDetails.genre}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <a href={imdbUrl} target="_blank" rel="noopener noreferrer">
+            Search IMDB
+          </a>
+        </Col>
+        <Col>
+          <a href={origin} target="_blank" rel="noopener noreferrer">
+            {originalID ? "Original data" : ""}
+          </a>
+        </Col>
+      </Row>
 
-    <Col xs={6} className="updateFilm">
-      <AdminUpdateFilm form={`updateImdb_${_id}`} imdbID={imdbID} onSubmit={handleSubmit} />
-    </Col>
+      <Col xs={6} className="updateFilm">
+        <AdminUpdateFilm
+          form={`updateImdb_${_id}`}
+          imdbID={imdbID}
+          onSubmit={handleSubmit}
+        />
+      </Col>
 
-    <Col sm={6} md={6} className="details">
-      <Image src={img} thumbnail responsive className="pull-right" />
+      <Col sm={6} md={6} className="details">
+        <Image src={img} thumbnail responsive className="pull-right" />
+      </Col>
     </Col>
-  </Col>
-)
-}
+  );
+};
 
 AdminFilm.propTypes = {
   title: PropTypes.string.isRequired
-}
+};
 
-export default AdminFilm
+export default AdminFilm;
--- frontend/src/components/Film.js
+++ frontend/src/components/Film.js
@@ -1,64 +1,84 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Row, Col, Panel } from 'react-bootstrap'
-import MapShowtimes from '../containers/MapShowtimes'
-import FontAwesome from 'react-fontawesome'
-
-const Film = ({ onClick, onFilmSave, title, imdbID, img, details, showtimes, reviewed, favorite }) => {
-  let filmDetails = (details) ? details : {};
-  let imagePath = (img !== 'N/A') ? img : '/image_not_found.jpg';
+import React from "react";
+import PropTypes from "prop-types";
+import { Row, Col, Panel } from "react-bootstrap";
+import MapShowtimes from "../containers/MapShowtimes";
+import FontAwesome from "react-fontawesome";
+const Film = ({
+  onClick,
+  onFilmSave,
+  title,
+  imdbID,
+  img,
+  details,
+  showtimes,
+  reviewed,
+  favorite
+}) => {
+  let filmDetails = details ? details : {};
+  let imagePath = img !== "N/A" ? img : "/image_not_found.jpg";
   // don't show films if there's no times to display
   if (!showtimes.length) {
-    return null
+    return null;
   }
-  let detailsStyle = "allDetails small" + (reviewed ? '' : ' reviewed') // the reviewed class greys things out
+  let detailsStyle = "allDetails small" + (reviewed ? "" : " reviewed"); // the reviewed class greys things out
 
   return (
-  <Panel header={title}>
-    {filmDetails.aka && <div className="altTitle">{filmDetails.aka}</div>}
-    <Col xs={4} className={detailsStyle}>
+    <Panel header={title}>
+      {filmDetails.aka && <div className="altTitle">{filmDetails.aka}</div>}
+      <Col xs={4} className={detailsStyle}>
         <Row>
-            <Col xs={8} className="details">
-                {filmDetails.genre}
-            </Col>
-            <span>{filmDetails.year}</span>
+          <Col xs={8} className="details">
+            {filmDetails.genre}
+          </Col>
+          <span>{filmDetails.year}</span>
         </Row>
         <Row>
-            <Col xs={8} className="details">
-                {filmDetails.director ? 'Director: ' + filmDetails.director : null}
-            </Col>
-            <span>{filmDetails.rating}</span>
+          <Col xs={8} className="details">
+            {filmDetails.director ? "Director: " + filmDetails.director : null}
+          </Col>
+          <span>{filmDetails.rating}</span>
         </Row>
         <Row>
-            <Col xs={12} className="details small">
-                {filmDetails.actors}
-            </Col>
-            <span className="language">
-              {(filmDetails.language && filmDetails.language.indexOf("English") !== -1) ? null : filmDetails.language}
-            </span>
+          <Col xs={12} className="details small">
+            {filmDetails.actors}
+          </Col>
+          <span className="language">
+            {filmDetails.language &&
+            filmDetails.language.indexOf("English") !== -1
+              ? null
+              : filmDetails.language}
+          </span>
         </Row>
         <Row>
-            <Col xs={8} className="details">
-                <em>{filmDetails.description}</em>
-            </Col>
-            <img className="img-responsive img-thumbnail pull-right" alt="" src={imagePath} />
+          <Col xs={8} className="details">
+            <em>{filmDetails.description}</em>
+          </Col>
+          <img
+            className="img-responsive img-thumbnail pull-right"
+            alt=""
+            src={imagePath}
+          />
         </Row>
         <Row className="filter favorite" onClick={onFilmSave}>
-          <FontAwesome name={(favorite) ? 'star' : 'star-o'} className={favorite && 'starred'}/>
-          <span className="filterText">{(favorite) ? 'saved' : 'save'}</span>
+          <FontAwesome
+            name={favorite ? "star" : "star-o"}
+            className={favorite && "starred"}
+          />
+          <span className="filterText">{favorite ? "saved" : "save"}</span>
         </Row>
         <Row className="filter filterOut" onClick={onClick}>
-          <FontAwesome name='ban' />
+          <FontAwesome name="ban" />
           <span className="filterText">filter</span>
         </Row>
-    </Col>
-    <div className="panel-body col-xs-8">
-      <div className="panel panel-info">
-        <MapShowtimes showtimes={showtimes} />
+      </Col>
+      <div className="panel-body col-xs-8">
+        <div className="panel panel-info">
+          <MapShowtimes showtimes={showtimes} />
+        </div>
       </div>
-    </div>
-  </Panel>
-)}
+    </Panel>
+  );
+};
 
 Film.propTypes = {
   onClick: PropTypes.func.isRequired,
@@ -66,6 +86,6 @@ Film.propTypes = {
   title: PropTypes.string.isRequired
   // details: PropTypes.object.isRequired,
   // createdAt: PropTypes.number.isRequired
-}
+};
 
-export default Film
+export default Film;
