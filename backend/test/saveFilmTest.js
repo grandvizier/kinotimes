c5a4f68d7a195f69b8891b77c93c834b29c4c6df
grandvizier <knurek.j@gmail.com>
2019-01-12 18:18:02 +0100

    enhance test with a second day save, and new assertion library

--- backend/test/saveFilmTest.js
+++ backend/test/saveFilmTest.js
@@ -1,5 +1,9 @@
 var assert = require('assert');
 var sinon = require('sinon');
+var chai = require("chai");
+var sinonChai = require("sinon-chai");
+chai.should();
+chai.use(sinonChai);
 
 var Database = require('../core/Database.js');
 var dbStub = sinon.spy(function() {
@@ -22,18 +26,41 @@ var t = {
 			origID: '/kino/_bin/filmdetail.php/12345/'
 		}]
 	},
-	date: "2019-01-01",
+	nextDayFilmData: {
+		name: 'Diff Theater',
+		films: [{
+			title: 'Tomorrow',
+			times: ['12:45', '18:00'],
+			origID: '/kino/_bin/filmdetail.php/12345/'
+		}]
+	},
+	date1: "2019-01-01",
+	date2: "2019-01-02",
 	showtime1: { _theater: '123', _film: '456', timestamp: '2019-01-01T18:45:00.000+00:00' },
 	showtime2: { _theater: '123', _film: '456', timestamp: '2019-01-01T20:30:00.000+00:00' },
+	nextDay1: { _theater: '321', _film: '654', timestamp: '2019-01-02T11:45:00.000+00:00' },
+	nextDay2: { _theater: '321', _film: '654', timestamp: '2019-01-02T17:00:00.000+00:00' },
 	theaterModel: {
 		name: 'Theater Name',
 		_id: '123',
+		showtimes: []
+	},
+	theaterModel2: {
+		name: 'Diff Theater',
+		_id: '321',
 		showtimes: []
 	},
 	filmModel: {
 		_id: '456',
 		title: 'Film Title  (OmU)',
 		originalID: '12345',
+		reviewed: false,
+		details: {}
+	},
+	filmModel2: {
+		_id: '654',
+		title: 'Tomorrow',
+		originalID: '54321',
 		reviewed: false,
 		details: {}
 	},
@@ -62,18 +89,18 @@ describe('SaveFilms', function() {
 			db.getFilm.yields(null, t.filmModel);
 			db.saveShowtime.yields(null, true);
 
-			saver.save([t.singleFilmData], t.date, successCb);
+			saver.save([t.singleFilmData], t.date1, successCb);
 
 			assert(successCb.calledOnce);
-			assert(db.saveShowtime.calledWith(t.showtime1));
-			assert(db.saveShowtime.calledWith(t.showtime2));
+			db.saveShowtime.should.have.been.calledWith(t.showtime1);
+			db.saveShowtime.should.have.been.calledWith(t.showtime2);
 		});
 
 		it('should catch db film errors', function() {
 			db.getTheater.yields(null, t.theaterModel);
 			db.getFilm.yields(t.errorStr, t.filmModel);
 
-			saver.save([t.singleFilmData], t.date, successCb);
+			saver.save([t.singleFilmData], t.date1, successCb);
 
 			assert(successCb.calledOnce);
 			assert(successCb.calledWith(t.errorStr));
@@ -86,10 +113,27 @@ describe('SaveFilms', function() {
 			db.saveShowtime.withArgs(t.showtime1).yields(null, true);
 			db.saveShowtime.withArgs(t.showtime2).yields(t.errorStr);
 
-			saver.save([t.singleFilmData], t.date, successCb);
+			saver.save([t.singleFilmData], t.date1, successCb);
 
 			assert(successCb.calledOnce);
 			assert(successCb.calledWith(t.errorStr));
+		});
+
+		it('should save multiple days', function() {
+			db.getTheater.withArgs(t.theaterModel.name).yields(null, t.theaterModel);
+			db.getTheater.withArgs(t.theaterModel2.name).yields(null, t.theaterModel2);
+			db.getFilm.onFirstCall().yields(null, t.filmModel);
+			db.getFilm.onSecondCall().yields(null, t.filmModel2);
+			db.saveShowtime.yields(null, true);
+
+			saver.save([t.singleFilmData], t.date1, successCb);
+			saver.save([t.nextDayFilmData], t.date2, successCb);
+
+			successCb.should.have.been.calledTwice;
+			db.saveShowtime.should.have.been.calledWith(t.showtime1);
+			db.saveShowtime.should.have.been.calledWith(t.showtime2);
+			db.saveShowtime.should.have.been.calledWith(t.nextDay1);
+			db.saveShowtime.should.have.been.calledWith(t.nextDay2);
 		});
 	});
 });
