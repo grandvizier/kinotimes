8dd7311c8d0ef41f74e6daa35733c4ca3978c4d3
grandvizier <knurek.j@gmail.com>
2019-03-10 13:23:50 +0100

    add function to save the original details to the db

--- backend/core/UpdateFilmInfo.js
+++ backend/core/UpdateFilmInfo.js
@@ -143,6 +143,62 @@ UpdateFilmInfo.prototype.imdbUpdateById = function(callback) {
 	});
 }
 
+
+UpdateFilmInfo.prototype.originalDataUpdate = function(filmId, filmInfo, callback) {
+	db.getFilm({'_id': filmId}, function(err, filmModel){
+		if(err){
+			logger.error("Can't find film to update", err);
+			return callback(err);
+		}
+		filmToUpdate = {'title': filmModel.title}
+		// TODO or if second scan already saved
+		if(filmModel.reviewed || filmModel.scannedOriginal) {
+			logger.debug(filmModel.title, "has already been reviewed/scanned")
+			return callback(null);
+		}
+		if(filmModel.imdbID){
+			// if country & year are set, AND they match what is already provided, mark the film as reviewed
+			if(filmInfo.country && filmInfo.year) {
+				if(filmModel.details.country && filmModel.details.year) {
+					if(filmModel.details.country == filmInfo.country && filmModel.details.year == filmInfo.year) {
+						filmToUpdate.reviewed = true
+					} else {
+						// sometimes the years are off by 1, that usually is close enough
+						yearDiff = parseInt(filmModel.details.year) - parseInt(filmInfo.year)
+						if (-1 <= yearDiff && yearDiff <= 1) {
+							filmToUpdate.reviewed = true
+						}
+					}
+				}
+			}
+			// if year & director are set, AND they match what is already provided, mark the film as reviewed
+			if(filmInfo.director && filmInfo.year) {
+				if(filmModel.details.director && filmModel.details.year) {
+					if(filmModel.details.director == filmInfo.director && filmModel.details.year == filmInfo.year) {
+						filmToUpdate.reviewed = true
+					} else if (!filmToUpdate.reviewed) {
+						logger.warn("new data doesn't match what is expected for", filmModel.title)
+						console.log(filmInfo)
+						console.log(filmModel.details.director, filmModel.details.year, filmModel.details.country)
+						// if they are set and they DO NOT match what is already provided, remove provided values
+						return callback(null);
+					}
+				}
+			}
+		}
+		// if there is no existing data, just add the info directly
+		filmToUpdate.scannedOriginal = true;
+		filmToUpdate.originalDetails = {
+			'director': filmInfo.director,
+			'country': filmInfo.country,
+			'year': filmInfo.year,
+		};
+		db.saveFilmInfo(filmToUpdate, function(err, saved){
+			callback(err);
+		});
+	});
+}
+
 UpdateFilmInfo.prototype.removeShowtimes = function(callback) {
 	db.removeShowtimesEverywhere(function(err){
 		callback(err);
