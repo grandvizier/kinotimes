937ba0097d5aaf2919d36c077549d68370af8192
grandvizier <grandvizier@users.noreply.github.com>
2020-01-03 18:57:41 +0100

    FRONTEND: refactor the film layout to use material-ui Card (#28)
    * fixes the bug where couldn't copy film titles

--- frontend/src/components/Film.tsx
+++ frontend/src/components/Film.tsx
@@ -1,33 +1,26 @@
-import * as React from 'react'
+import React from 'react'
 import {
     Typography,
     Grid,
-    ExpansionPanel,
-    ExpansionPanelSummary,
-    ExpansionPanelDetails
+    Card,
+    CardContent,
+    CardActions,
+    Collapse,
+    IconButton
 } from "@material-ui/core";
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+
 import MapShowtimes from "../containers/MapShowtimes";
 import {ShowTime} from "../types";
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import BanIcon from "@material-ui/icons/Warning";
+// import BanIcon from "@material-ui/icons/Warning";
 // import StarOffIcon from "@material-ui/icons/StarOutlined";
 import StarOnIcon from "@material-ui/icons/Star";
 
+
 const styles = {
-    root: {
-        flexGrow: 1,
-        overflow: 'hidden',
-        padding: `0 30px`,
-    },
-    paper: {
-        maxWidth: '90%',
-        margin: `10px auto`,
-        backgroundColor: `rgba(168, 166, 173,0.9)`,
-    },
-    title: {
-        // color: '#000000',
-    },
     contentTitle: {
         // color: '#000000',
         fontWeight: 600,
@@ -45,109 +38,146 @@ const styles = {
         fontSize: 'small',
         fontStyle: 'italic',
     },
+};
+
+const useStyles = makeStyles(theme => ({
+    card: {
+        margin: '50px',
+        backgroundColor: `rgba(0, 0, 0, 0.8)`,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+          duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    title: {
+        color: `#bf997e`,
+    },
     language: {
         color: 'red',
         fontWeight: 400,
         fontSize: 'small',
     },
-    panelHeader: {
-        borderBottom: '1px solid rgba(0,0,0,.125)',
-        backgroundColor: `rgba(0, 0, 0, 0.6)`,
+    rating: {
         color: `#bf997e`,
+        width: '50px',
+        height: '50px',
+        borderRadius: '50%',
+        lineHeight: '50px',
+        textAlign: 'center',
+        border: 'white solid 2px'
     },
     showtimeContent: {
-        backgroundImage: 'linear-gradient(rgba(35,35,35,0.5) 20%, rgba(25,25,25,0))',
+        backgroundColor: `rgba(0, 0, 0, 0.1)`,
+        backgroundImage: 'linear-gradient(rgba(35,35,35,0.2) 20%, rgba(25,25,25,0))',
         color: `#bf997e`,
     }
-};
+}));
 
-export interface Props {
+type FilmProps = {
     onFilterClick: () => void;
     onFilmSave: () => void;
     title: string;
     img: string;
     // what is details reviewed and favorite type
+    // https://www.robinwieruch.de/react-pass-props-to-component
     details: any;
     showtimes: ShowTime[];
     favorite: any;
 }
 
-class Film extends React.Component<Props> {
+// is there a cleaner way to pass in the props
+// https://www.robinwieruch.de/react-function-component
+export default function Film({
+    onFilterClick,
+    onFilmSave,
+    title,
+    img,
+    details,
+    showtimes,
+    favorite
+}: FilmProps) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
 
-    render() {
-        const {
-            onFilterClick,
-            onFilmSave,
-            title,
-            img,
-            details,
-            showtimes,
-            favorite
-        } = this.props;
-
-        let filmDetails = details ? details : {};
-        let imagePath = img !== "N/A" ? img : "/image_not_found.jpg";
-        // don't show films if there's no times to display
-        if (!showtimes.length) {
-            return null;
-        }
-        let showLanguage = filmDetails.language && filmDetails.language.indexOf("English") === -1
-        // let detailsStyle = classNames("", {reviewed: !reviewed}); // if not reviewed, grey things out
-
-
-        // https://material-ui.com/components/cards/
-        return (
-            <div style={styles.root}>
-                <ExpansionPanel style={styles.paper} square>
-                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>} style={styles.panelHeader}>
-                        <Grid container justify="space-evenly" alignItems="flex-start">
-                            <Grid container item xs={3} md={2}>
-                                <img alt={title} src={imagePath} className="small-thumbnail"    />
-                            </Grid>
-                            <Grid item xs={4} md={6}>
-                                <Grid>
-                                    <Typography style={styles.title}>{title} {filmDetails.aka &&
-                                    <div className="altTitle">({filmDetails.aka})</div>}</Typography>
-                                    <Typography style={styles.contentText}>{filmDetails.genre}</Typography>
-                                    <Typography style={styles.contentText}>{filmDetails.year}</Typography>
-                                    <Typography style={styles.contentText}>{filmDetails.rating}</Typography>
-                                    {filmDetails.director ? <Typography style={styles.contentTitle}>
-                                        Director: <span style={styles.contentText}>{filmDetails.director}</span></Typography> : null}
-
-                                    {filmDetails.actors ? <Typography style={styles.contentTitle}>
-                                        Actors: <span style={styles.contentText}>{filmDetails.actors}</span></Typography> : null}
-
-                                    {showLanguage ?
-                                        <Typography style={styles.language}>{filmDetails.language}</Typography> : null
-                                    }
-                                </Grid>
-                                <Grid>
-                                    <Typography variant="body1" style={styles.contentDesc}>{filmDetails.description}</Typography>
-                                </Grid>
-                            </Grid>
-                            <Grid container item xs={1} md={2}></Grid>
-                            <Grid container item xs={2} md={2}>
-                                <Grid item xs={8} className="filter favorite" onClick={onFilmSave}>
-                                    <StarOnIcon className={favorite && "starred"}/>
-                                    <Typography
-                                        className="filterText">{favorite ? "saved" : "save"}</Typography>
-                                </Grid>
-                                <Grid item xs={4} className="filter filterOut" onClick={onFilterClick}>
-                                    <BanIcon/>
-                                    <Typography className="filterText">filter</Typography>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails style={styles.showtimeContent}>
-                        <Grid container justify="space-evenly" alignItems="flex-start">
-                            <MapShowtimes showtimes={showtimes}/>
-                        </Grid>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-            </div>
-        );
+    // don't show films if there's no times to display
+    if (!showtimes.length) {
+        return null;
     }
+
+    let filmDetails = details ? details : {};
+    let imagePath = img !== "N/A" ? img : "/image_not_found.jpg";
+    let showLanguage = filmDetails.language && filmDetails.language.indexOf("English") === -1;
+
+    const bull = <span className={classes.bullet}>•</span>;
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    // TODO
+    //  add divider(s) where needed
+    // https://material-ui.com/components/dividers/
+
+    return (
+    <Card className={classes.card}>
+        <CardContent>
+            <Grid container>
+                <Grid item xs={3} md={2}>
+                    <img alt={title} src={imagePath} className="small-thumbnail" />
+                </Grid>
+                <Grid item xs={4} md={6}>
+                    <Typography className={classes.title}>{title} {filmDetails.aka &&
+                        <span className="altTitle">({filmDetails.aka})</span>}</Typography>
+                    <Typography>
+                        {filmDetails.year}{bull}{filmDetails.genre}
+                    </Typography>
+                    {showLanguage ? <Typography className={classes.language}>{filmDetails.language}</Typography> : null}
+                    {filmDetails.director ? <Typography style={styles.contentTitle}>
+                        Director: <span style={styles.contentText}>{filmDetails.director}</span></Typography> : null}
+
+                    {filmDetails.actors ? <Typography style={styles.contentTitle}>
+                        Actors: <span style={styles.contentText}>{filmDetails.actors}</span></Typography> : null}
+
+                    <Typography variant="body2" style={styles.contentDesc}>{filmDetails.description}</Typography>
+                </Grid>
+                <Grid item xs={1} md={2}>
+                    <Typography className={classes.rating}>{filmDetails.rating}</Typography>
+                </Grid>
+                <Grid item xs={2} md={2}>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <StarOnIcon />
+                        </IconButton>
+                        <IconButton
+                              className={clsx(classes.expand, {
+                                [classes.expandOpen]: expanded,
+                              })}
+                              onClick={handleExpandClick}
+                              aria-expanded={expanded}
+                              aria-label="show more"
+                            >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                </Grid>
+            </Grid>
+        </CardContent>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent className={classes.showtimeContent}>
+                <MapShowtimes showtimes={showtimes}/>
+            </CardContent>
+        </Collapse>
+    </Card>
+    );
 }
 
-export default Film;
